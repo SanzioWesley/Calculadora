df4a8cd8c3887c2e6831c0120a3bcb6f1e3e30df
sanzio <sanziowesley@hotmail.com>
2019-12-26 19:34:27 -0300

    up

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Sanzio o gatão
+        </p>
+        <h1
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
