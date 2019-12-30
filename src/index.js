7c04e252675979dc7e6313e662522f8825f01254
sanzio <sanziowesley@hotmail.com>
2019-12-30 11:11:02 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( < div > < h1 > Calculator < /h1><Calculator / > < /div>, document.getElementById('root'));
-        serviceWorker.unregister();
+ReactDOM.render(
+        <div>
+                <h1>Calculator </h1><Calculator />
+        </div>
+        , document.getElementById('root'));
+serviceWorker.unregister();
--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -28,7 +28,29 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation) {
-        console.log(operation)
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e) {
+                values[0] = this.state.values[0]
+            }
+           
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
 
     addDigit(n) {
@@ -57,7 +79,7 @@ export default class Calculator extends Component {
         const setOperation = op => this.setOperation(op)
         return (
             <div className="calculator">
-                <Display value={0} />
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click={this.clearMemory} triple />
                 <Button label="/" click={this.setOperation} operation />
                 <Button label="7" click={this.addDigit} />
